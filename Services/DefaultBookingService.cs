cf8996217269a6ec07ce7f64a397da3cd8f3a6c1
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-12-22 14:49:37 -0600

    Finished form posting logic that creates the booking.

--- Services/DefaultBookingService.cs
+++ Services/DefaultBookingService.cs
@@ -24,14 +24,44 @@ namespace DemoApi.Services
 			_mapper = mapper;
 		}
 
-		public Task<Guid> CreateBookingAsync(
+		public async Task<Guid> CreateBookingAsync(
 			Guid userId,
 			Guid roomId,
 			DateTimeOffset startAt,
 			DateTimeOffset endAt)
 		{
-			// TODO: Save the new booking to the database
-			throw new NotImplementedException();
+			// Lookup the room reference to do it.
+			var room = await _context.Rooms.SingleOrDefaultAsync(r => r.Id == roomId);
+			// Error out if required.
+			if (room == null)
+            {
+				throw new ArgumentException("Invalid room ID");
+            }
+
+			// Calculate how much 
+			var minimumStay = _dateLogicService.GetMinimumStay();
+			var total = (int)((endAt - startAt).TotalHours / minimumStay.TotalHours) * room.Rate;
+
+			var bookingId = Guid.NewGuid();
+
+			var newBooking = _context.Bookings.Add(new BookingEntity
+			{
+				Id = bookingId,
+				CreatedAt = DateTimeOffset.UtcNow,
+				ModifiedAt = DateTimeOffset.UtcNow,
+				StartAt = startAt.ToUniversalTime(),
+				EndAt = endAt.ToUniversalTime(),
+				Total = total,
+				Room = room
+			});
+
+			var created = await _context.SaveChangesAsync();
+			if (created < 1)
+            {
+				throw new InvalidOperationException("Could not create the booking.");
+            }
+
+			return bookingId;
 		}
 
 		public async Task<Booking> GetBookingAsync(Guid bookingId)
